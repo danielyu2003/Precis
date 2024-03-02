f17ce139dadd41d60dafbef628b539dea42d2ee9
Daniel Yu <88413927+danielyu2003@users.noreply.github.com>
2024-03-02 16:19:33 -0500

    Added ArgumentParser and renamed main file

---
+++ Precis/Precis.swift
@@ -0,0 +1,15 @@
+//
+//  Precis.swift
+//  Precis
+//
+//  Created by Daniel Yu on 3/1/24.
+//
+
+import ArgumentParser
+
+@main
+struct Precis: ParsableCommand {
+	mutating func run() throws {
+		print("Hello, World!")
+	}
+}
